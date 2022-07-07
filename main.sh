71b58a3403e5f652b0c331a478ace9b672e49866
Delin Qu <qdl.cs@qq.com>
2022-07-07 17:34:06 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,18 @@
+mkdir output/gs
+mkdir output/rs
+
+# RS simulator
+seq=01
+echo $seq
+sudo docker run --rm --interactive \
+  --user $(id -u):$(id -g)      \
+  --volume "$(pwd):/kubric"     \
+  kubricdockerhub/kubruntu      \
+  /usr/bin/python3 rs/model.py  \
+  --use_motion_blur=1           \
+  --out_dir=output/rs/seq$seq/    \
+  --pose=./rs/data/pose$seq.csv \
+  --write=1
+
+# GS simulator
+
